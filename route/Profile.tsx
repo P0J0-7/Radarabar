89938f16f7b61ccbaa65fa157a022eefac1dce55
P0J0-7 <joshualucas85000@gmail.com>
2023-02-08 14:37:31 +0100

    add files

---
+++ route/Profile.tsx
@@ -0,0 +1,23 @@
+/* eslint-disable react-native/no-inline-styles */
+import React, {useState} from 'react';
+import {Text, TextInput, View} from 'react-native';
+
+export default function Profile() {
+  const [text, setText] = useState('');
+  return (
+    <View style={{padding: 10}}>
+      <TextInput
+        style={{height: 40}}
+        placeholder="Type here to translate!"
+        onChangeText={newText => setText(newText)}
+        defaultValue={text}
+      />
+      <Text style={{padding: 10, fontSize: 42}}>
+        {text
+          .split(' ')
+          .map(word => word && '🍕')
+          .join(' ')}
+      </Text>
+    </View>
+  );
+}
