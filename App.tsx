1ac465bca348bbdc91216d7400b53aec5c643703
P0J0-7 <124791604+P0J0-7@users.noreply.github.com>
2023-02-12 14:28:51 +0100

    Updates

--- App.tsx
+++ App.tsx
@@ -1,69 +1,120 @@
 /* eslint-disable react/no-unstable-nested-components */
-import * as React from 'react';
+import React, { useState } from "react";
 import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
 import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
 import {NavigationContainer} from '@react-navigation/native';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
 
-import Home from './route/Home';
-import Favorite from './route/Favorite';
-import Profile from './route/Profile';
-import Settings from './route/Settings';
+import MainMenu from './MainMenu';
 
+
+import {
+  StyleSheet,
+  Text,
+  View,
+  ImageBackground, 
+  Image,
+  TextInput,
+  TouchableOpacity,
+} from "react-native";
+
+import Background from './images/Background_color_main.png';
+
+const BACKGROUND = Image.resolveAssetSource(Background).uri;
 const Tab = createBottomTabNavigator();
+const Stack = createNativeStackNavigator();
+
 export default function App() {
   return (
-    <NavigationContainer>
-      <Tab.Navigator
-        initialRouteName="Feed"
-        screenOptions={{
-          tabBarActiveTintColor: '#701E3A',
-          headerShown: false,
-        }}>
-        <Tab.Screen
-          name="Home"
-          component={Home}
-          options={{
-            tabBarLabel: 'Home',
-            tabBarIcon: ({color, size}) => (
-              <MaterialCommunityIcons name="home" color={color} size={size} />
-            ),
-          }}
-        />
-        <Tab.Screen
-          name="Favorite"
-          component={Favorite}
-          options={{
-            tabBarLabel: 'Favorite',
-            tabBarIcon: ({color, size}) => (
-              <MaterialCommunityIcons name="heart" color={color} size={size} />
-            ),
-          }}
-        />
-        <Tab.Screen
-          name="Profile"
-          component={Profile}
-          options={{
-            tabBarLabel: 'Profile',
-            tabBarIcon: ({color, size}) => (
-              <MaterialCommunityIcons
-                name="account"
-                color={color}
-                size={size}
-              />
-            ),
-          }}
-        />
-        <Tab.Screen
-          name="Settings"
-          component={Settings}
-          options={{
-            tabBarLabel: 'Settings',
-            tabBarIcon: ({color, size}) => (
-              <MaterialCommunityIcons name="wrench" color={color} size={size} />
-            ),
-          }}
-        />
-      </Tab.Navigator>
+    <NavigationContainer independent={true}>
+      <Stack.Navigator initialRouteName="Login" screenOptions={{headerShown: false}}>
+        <Stack.Screen name="Login" component={Login} />
+        <Stack.Screen name="Main" component={MainMenu} />
+      </Stack.Navigator>
     </NavigationContainer>
   );
 }
+
+
+export function Login({navigation}) {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  return (
+    <View style={{flex: 1, backgroundColor: '#F8F8F8'}}>
+      <ImageBackground
+        resizeMode="cover"
+        source={{uri: BACKGROUND}}
+        style={{flex: 1, marginTop:-100 ,justifyContent: 'center'}}>
+        <Text
+          style={{
+            flex: 1,
+            marginTop: 250,
+            marginLeft: 35,
+            fontSize: 32,
+            color: 'black',
+            fontWeight: 'bold',
+          }}>
+          On sort où ce soir ?
+        </Text>
+        <View style={styles.container}>
+        <View style={styles.inputView}>
+          <TextInput
+            style={styles.TextInput}
+            placeholder="Adresse mail"
+            placeholderTextColor="grey"
+            onChangeText={(email) => setEmail(email)}
+          />
+        </View>
+        <View style={styles.inputView}>
+          <TextInput
+            style={styles.TextInput}
+            placeholder="Mot de passe"
+            placeholderTextColor="grey"
+            secureTextEntry={true}
+            onChangeText={(password) => setPassword(password)}
+          />
+        </View>
+        <TouchableOpacity style={styles.loginBtn} onPress={() => navigation.navigate('Main')} >
+          <Text style={{color: 'lightgrey', fontWeight: 'bold'}}>LOGIN</Text>
+        </TouchableOpacity>
+        <TouchableOpacity>
+          <Text style={styles.forgot_button}>Forgot Password?</Text>
+        </TouchableOpacity>
+      </View>
+      </ImageBackground>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: "center",
+  },
+  inputView: {
+    backgroundColor: "#FFFFFF",
+    borderRadius: 30,
+    width: "70%",
+    height: 45,
+    marginBottom: 20,
+  },
+  TextInput: {
+    height: 50,
+    flex: 1,
+    padding: 10,
+    marginLeft: 20,
+  },
+  forgot_button: {
+    height: 30,
+    marginTop: 30,
+  },
+  loginBtn: {
+    width: "80%",
+    borderRadius: 25,
+    height: 50,
+    alignItems: "center",
+    justifyContent: "center",
+    backgroundColor: "#701E3A",
+  },
+});
+
---
+++ MainMenu.tsx
@@ -0,0 +1,70 @@
+/* eslint-disable react/no-unstable-nested-components */
+import * as React from 'react';
+import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
+import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
+import {NavigationContainer} from '@react-navigation/native';
+
+import Home from './route/Home';
+import Favorite from './route/Favorite';
+import Profile from './route/Profile';
+import Settings from './route/Settings';
+
+const Tab = createBottomTabNavigator();
+
+export default function MainMenu() {
+  return (
+    <NavigationContainer independent={true}>
+      <Tab.Navigator
+        initialRouteName="Feed"
+        screenOptions={{
+          tabBarActiveTintColor: '#701E3A',
+          headerShown: false,
+        }}>
+        <Tab.Screen
+          name="Home"
+          component={Home}
+          options={{
+            tabBarLabel: 'Home',
+            tabBarIcon: ({color, size}) => (
+              <MaterialCommunityIcons name="home" color={color} size={size} />
+            ),
+          }}
+        />
+        <Tab.Screen
+          name="Favorite"
+          component={Favorite}
+          options={{
+            tabBarLabel: 'Favorite',
+            tabBarIcon: ({color, size}) => (
+              <MaterialCommunityIcons name="heart" color={color} size={size} />
+            ),
+          }}
+        />
+        <Tab.Screen
+          name="Profile"
+          component={Profile}
+          options={{
+            tabBarLabel: 'Profile',
+            tabBarIcon: ({color, size}) => (
+              <MaterialCommunityIcons
+                name="account"
+                color={color}
+                size={size}
+              />
+            ),
+          }}
+        />
+        <Tab.Screen
+          name="Settings"
+          component={Settings}
+          options={{
+            tabBarLabel: 'Settings',
+            tabBarIcon: ({color, size}) => (
+              <MaterialCommunityIcons name="wrench" color={color} size={size} />
+            ),
+          }}
+        />
+      </Tab.Navigator>
+    </NavigationContainer>
+  );
+}
