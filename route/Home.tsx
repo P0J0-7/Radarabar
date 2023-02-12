2eaf7fccd897664d8c8c84ae1cfa521a2c769abf
P0J0-7 <joshualucas85000@gmail.com>
2023-02-12 11:06:33 +0100

    modifying code and images

--- route/Home.tsx
+++ route/Home.tsx
@@ -1,20 +1,22 @@
 /* eslint-disable react-native/no-inline-styles */
 import React from 'react';
-import {Text, ImageBackground,Image, View} from 'react-native';
-import Logo from '../images/Radarabar_logo_P.png';
+import {Text, ImageBackground, Image, View} from 'react-native';
 import Background from '../images/Background_color_main.png';
 
-const LOGO = Image.resolveAssetSource(Logo).uri;
 const BACKGROUND = Image.resolveAssetSource(Background).uri;
 
 export default function Home() {
   return (
-    <View style={{padding: 20, backgroundColor: '#F8F8F8'}}>
-      <ImageBackground resizeMode="cover" source={{uri: BACKGROUND}}>
-        <Image style={{width: 300, height: 100}} source={{uri: LOGO}} />
+    <View style={{flex: 1, backgroundColor: '#F8F8F8'}}>
+      <ImageBackground
+        resizeMode="cover"
+        source={{uri: BACKGROUND}}
+        style={{flex: 1, justifyContent: 'center'}}>
         <Text
           style={{
-            padding: 10,
+            flex: 1,
+            marginTop: 180,
+            marginLeft: 35,
             fontSize: 32,
             color: 'black',
             fontWeight: 'bold',
